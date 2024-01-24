cda59bb01a729c1b352d14ec99dd2f123d3019f3
Rahul Sen <rsen253@gmail.com>
2024-01-24 16:04:29 +0530

    Created separate card component - styled the component - Added new font for the title

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import Card from "../card/card.component";
 import "./card-list.styles.css";
 class CardList extends Component {
   render() {
@@ -6,17 +7,7 @@ class CardList extends Component {
     return (
       <div className="card-list">
         {monsters.map((monster) => {
-          const { id, name, email } = monster;
-          return (
-            <div className="card-container" key={id}>
-              <img
-                alt={`monster ${monster.name}`}
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          );
+          return <Card monster={monster} key={monster.id} />;
         })}
       </div>
     );
